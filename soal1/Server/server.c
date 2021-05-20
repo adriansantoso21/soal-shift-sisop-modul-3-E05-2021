bd081b8387bf072ca5b39886455379f8c2192fe3
adriansantoso21 <65168221+adriansantoso21@users.noreply.github.com>
2021-05-21 02:02:56 +0700

    Update server.c

--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -31,6 +31,83 @@ char status[1000], message[2000], message1[2000];
 akun client_akun[1000];
 buku client_buku[1000];
 
+char *checkfile(char *filename) {
+    if(filename[0] == '.') return "Hidden"; 
+    char *temp = strchr(filename, '.');
+    if(!temp) return "Unknown";
+    return temp + 1;
+}
+
+void deletefile(char nama_file[]){
+
+	int iter=-1;
+	char pathtujuan[1000], pathasal[1000], temp[1000];
+  	memset(pathasal, 0, 1000);
+  	strcpy(pathasal, "/home/adr01/Documents/SesiLab3/Soal1/Server/FILES/");
+  	strcat(pathasal, nama_file);
+  	 
+  	memset(pathtujuan, 0, 1000);
+  	strcpy(pathtujuan, "/home/adr01/Documents/SesiLab3/Soal1/Server/FILES/");
+  	strcat(pathtujuan, "old-");
+  	strcat(pathtujuan, nama_file);
+  	
+  	// Memindahkan folder
+  	rename(pathasal, pathtujuan);
+  	
+  	// Menghapus dari array
+  	for (int a=0; a<=jumlah_buku; a++){
+  		if(strcmp(nama_file, client_buku[a].nama)==0) continue;
+  		
+  		strcpy(client_buku[++iter].publisher, client_buku[a].publisher);
+		strcpy(client_buku[++iter].path, client_buku[a].path);
+		strcpy(client_buku[++iter].tahun, client_buku[a].tahun);
+		strcpy(client_buku[++iter].nama, client_buku[a].nama);
+		strcpy(client_buku[++iter].extension, client_buku[a].extension);
+  	}
+  	
+  	jumlah_buku--;
+  	
+  	// Menghapus dari file tsv
+	FILE *filetsv;
+    filetsv = fopen("files.tsv", "w");
+    fprintf(filetsv, "Publisher\tTahun publikasi\tFilepath\n");
+    for(int a=0; a<=jumlah_buku; a++){
+    	strcpy(temp, client_buku[a].publisher);strcat(temp, "\t");
+    	strcat(temp, client_buku[a].tahun);strcat(temp, "\t");
+    	strcat(temp, client_buku[a].path);strcat(temp, "\n");
+    	memset(temp, 0, 1000);
+    }
+    fclose(filetsv);
+}
+
+void downloadfile(char nama_file[]){
+
+	char pathtujuan[1000], pathasal[1000];
+  	memset(pathasal, 0, 1000);
+  	strcpy(pathasal, "/home/adr01/Documents/SesiLab3/Soal1/Server/FILES/");
+  	strcat(pathasal, nama_file);
+  	 
+	// Mengambil data dari folder FILES	
+	FILE *file;
+	file = fopen(pathasal, "r");
+	
+	// Membuat file pada client
+  	memset(pathtujuan, 0, 1000);
+  	strcpy(pathtujuan, "/home/adr01/Documents/SesiLab3/Soal1/Client/");
+  	strcat(pathtujuan, nama_file);
+  	
+  	// Menuliskan isi folder
+	FILE *fp;
+	fp = fopen (pathtujuan, "w");
+	char temp = fgetc(file);
+	while(temp != EOF){
+		fputc(temp, fp);
+		temp = fgetc(file);
+	}
+	fclose(file);
+	fclose(fp);
+}
+
 char *FileExtension(char *filename) {
     char *temp = strrchr(filename, '.');
     return temp + 1;
@@ -40,8 +117,6 @@ void moveFiles(char file_path[], char nama_file[]){
 
 	// Mengambil data dari file asal	
 	FILE *file;
-	char line[10000], all[10000];
-	memset(line, 0, 10000);memset(all, 0, 10000);
 	
 	file = fopen(file_path, "r");
 	
@@ -125,6 +200,7 @@ void *multiple_connection (void *socket_desc)
 	else send(sock , "kosong" , 10 , 0 );
 	memset( message, 0, strlen(message) );
 	
+	first_login:;
 	// Menerima pesan dari user apakah dia login / register
 	while( (read_size = recv(sock , message , 2000 , 0)) > 0 )
 	{
@@ -150,19 +226,28 @@ void *multiple_connection (void *socket_desc)
 				memset( message, 0, strlen(message) );
 				strcpy(message, "Proses register berhasil\n");
 				send(sock , message , strlen(message) , 0 );
+				//goto first_login;
 			}
 			
 			// Jika pilihan login
 			else if (message[0] == 'l'){
-				status = login_user(id_user, pass_user);
+			
+				status = login_user(id_user, pass_user);	
+				if(status) {
+					send(sock , "true" , 1000 , 0 );
+					goto after_login;
+				}
+				else {
+					send(sock , "false" , 1000 , 0 );
+					//goto first_login;
+				}
 				
-				if(status) send(sock , "true" , 1000 , 0 );
-				else send(sock , "false" , 1000 , 0 );
-				
+				after_login:;
 				memset( message, 0, strlen(message) );
 				recv(sock , message , 2000 , 0);
-				if(message[0] == 'a') {
 				
+				// Jika pilihan add
+				if(message[0] == 'a') {
 					// Mengambil data publisher, tahun, path
 					int it1=-1, it2=-1, it3=-1, d, e, f;
 					char publisher[1000], tahun[1000], path[1000], namaFile[1000], fileExt[1000], temp[1000];
@@ -195,7 +280,15 @@ void *multiple_connection (void *socket_desc)
 					
 					add_tsvfile(message);
 					moveFiles(path, namaFile);
+					
+					// Menambahkan ke running.log
+					FILE *f1;
+					f1 = fopen("running.log","a");
+					fprintf(f1,"Tambah : %s (%s:%s)\n", namaFile, id_user, pass_user);
+					fclose(f1);
+					
 					send(sock , "File berhasil dimasukkan" , 1000 , 0 );
+					goto after_login;
 				}
 				
 				// Jika pilihan see
@@ -220,11 +313,121 @@ void *multiple_connection (void *socket_desc)
 							
 							strcpy(path, "Filepath: ");strcat(path, client_buku[a].path);strcat(path, "\n");
 							
-							printf("Ini nama : %s\n", namaFile);
-							printf("Ini publisher : %s\n", publisher);
-							printf("Ini tahun : %s\n", tahun);
-							printf("Ini ext : %s\n", fileExt);
-							printf("path : %s\n", path);
+							//printf("%s\n", publisher);
+							
+							send(sock , namaFile , strlen(namaFile) , 0 );
+							send(sock , publisher , strlen(publisher) , 0 );
+							send(sock , tahun , strlen(tahun) , 0 );
+							send(sock , fileExt , strlen(fileExt) , 0 );
+							send(sock , path , strlen(path) , 0 );
+							send(sock , "\n" , 1000 , 0 );
+						}
+						send(sock , "File berhasil ditampilkan\n" , 1000 , 0 );
+					}
+					goto after_login;
+				}
+				
+				// Jika pilihan delete
+				if(message[0] == 'h') {
+					char path_delete[1000];
+					int iter2 = -1;
+					int flag=0;
+
+					for(int g=1; g<= strlen(message); g++ ){
+						path_delete[++iter2] = message[g];
+					}
+					
+					// Cek file
+					for(int h = 0; h <=jumlah_buku; h++){
+						if(strcmp(path_delete, client_buku[h].nama)==0){
+							flag=1;
+							break;
+						}
+					}
+					
+					if(flag){
+						deletefile(path_delete);
+						
+						// Menambahkan ke running.log
+						FILE *f1;
+						f1 = fopen("running.log","a");
+						fprintf(f1,"Hapus : %s (%s:%s)\n", path_delete, id_user, pass_user);
+						fclose(f1);
+												
+						send(sock , "ok" , 3 , 0 );
+					}
+					else{
+						send(sock , "no" , 3 , 0 );
+					}
+					goto after_login;
+				}
+				
+				// Jika pilihan download
+				if(message[0] == 'd') {
+					char nama_file[1000], path_delete[1000], path_baru[1000];
+					int iter2 = -1;
+					int flag=0;
+
+					for(int g=1; g<= strlen(message); g++ ){
+						nama_file[++iter2] = message[g];
+					}
+					
+					// Cek file
+					for(int h = 0; h <=jumlah_buku; h++){
+						if(strcmp(nama_file, client_buku[h].nama)==0){
+							flag=1;
+							break;
+						}
+					}
+					
+					if(flag){
+						downloadfile(nama_file);
+						printf("Berhasil download\n");
+						send(sock , "ok" , 3 , 0 );
+					}
+					else{
+					printf("gagal download\n");
+						send(sock , "no" , 3 , 0 );
+					}
+					goto after_login;
+				}
+				
+				// Jika pilihan find
+				if(message[0] == 'f') {
+					char nama_file[1000];
+					int iter2 = -1;
+					int flag=0;
+
+					for(int g=1; g<= strlen(message); g++ ){
+						nama_file[++iter2] = message[g];
+					}
+					
+					printf("ini nama_file: %s\n", nama_file);
+					
+					if(jumlah_buku == -1) send(sock , "kosong" , 1000 , 0 );
+					else {
+						char publisher[1000], tahun[1000], path[1000], namaFile[1000], fileExt[1000], temp[1000];
+						for(int a=0; a<=jumlah_buku; a++){
+							printf("ini buku ke-%d : %s\n", a, client_buku[a].nama);
+							// Mengecek apakah terdapat atau tidak
+							if(strstr(client_buku[a].nama, nama_file) == NULL ) continue;
+							flag=1;
+							
+							memset( namaFile, 0, strlen(namaFile) );
+							memset( publisher, 0, strlen(publisher) );
+							memset( tahun, 0, strlen(tahun) );
+							memset( fileExt, 0, strlen(fileExt) );
+							memset( path, 0, strlen(path) );
+							
+							strcpy(namaFile, "Nama: ");strcat(namaFile, client_buku[a].nama);strcat(namaFile, "\n");
+							
+							strcpy(publisher, "Publisher: ");strcat(publisher, client_buku[a].publisher);strcat(publisher, "\n");
+							
+							strcpy(tahun, "Tahun publishing: ");strcat(tahun, client_buku[a].tahun);strcat(tahun, "\n");
+							
+							strcpy(fileExt, "Ekstensi file: ");strcat(fileExt, client_buku[a].extension);strcat(fileExt, "\n");
+							
+							strcpy(path, "Filepath: ");strcat(path, client_buku[a].path);strcat(path, "\n");
 							
 							send(sock , namaFile , 1000 , 0 );
 							send(sock , publisher , 1000 , 0 );
@@ -233,8 +436,16 @@ void *multiple_connection (void *socket_desc)
 							send(sock , path , 1000 , 0 );
 							send(sock , "\n" , 1000 , 0 );
 						}
-						send(sock , "File berhasil ditampilkan\n" , 1000 , 0 );
+						
+						if(flag){
+							send(sock , "ok" , 3 , 0 );
+						}
+						else{
+							send(sock , "no" , 3 , 0 );
+						}
 					}
+					
+					goto after_login;
 				}
 			}
 			
